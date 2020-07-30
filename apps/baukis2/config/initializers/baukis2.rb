0720ed0b69dac0668859ed4dffad57b26948c496
Asatogenki <asa.326.gen@gmail.com>
2020-07-30 16:54:57 +0900

    演習まで完了

--- apps/baukis2/config/initializers/baukis2.rb
+++ apps/baukis2/config/initializers/baukis2.rb
@@ -2,6 +2,6 @@ Rails.application.configure do
   config.baukis2 = {
     staff: { host: "baukis2.example.com", path: ""},
     admin: { host: "baukis2.example.com", path: "admin"},
-    custmer: { host: "example.com", path: "mypage" }
+    customer: { host: "example.com", path: "mypage" }
   }
 end
--- apps/baukis2/spec/routing/hostname_constraints_spec.rb
+++ apps/baukis2/spec/routing/hostname_constraints_spec.rb
@@ -21,13 +21,22 @@ describe "ルーティング" do
     )
   end
 
-  example "ホスト名が対象外ならroutableではない" do
-    expect(get: "http://foo.example.jp").not_to be_routable
+  example "顧客トップページ" do
+    config = Rails.application.config.baukis2
+    url = "http://#{config[:customer][:host]}/#{config[:customer][:path]}"
+    expect(get: url).to route_to(
+      host: config[:customer][:host],
+      controller: "customer/top",
+      action: "index"
+    )
   end
 
-  example "存在しないパスならroutebleではない" do
+  example "ホスト名が対象外ならroutableではない" do
+    expect(get: "http://foo.example.com").not_to be_routable
+  end
+
+  example "存在しないパスならroutableではない" do
     config = Rails.application.config.baukis2
     expect(get: "http://#{config[:staff][:host]}/xyz").not_to be_routable
   end
 end
-
