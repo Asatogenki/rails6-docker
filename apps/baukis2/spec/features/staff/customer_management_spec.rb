7429acd657eb6b3cb617e00b29d78c82500755d5
Asatogenki <asa.326.gen@gmail.com>
2020-08-21 23:47:35 +0900

    17.2　顧客アカウント新規登録・更新機能の改良 終了

--- apps/baukis2/spec/features/staff/customer_management_spec.rb
+++ apps/baukis2/spec/features/staff/customer_management_spec.rb
@@ -15,43 +15,19 @@ feature "職員による顧客管理" do
     first("div.links").click_link "新規登録"
 
     fill_in "メールアドレス", with: "test@example.jp"
-    fill_in "パスワード", with: "pw"
     fill_in "form_customer_family_name", with: "試験"
     fill_in "form_customer_given_name", with: "花子"
     fill_in "form_customer_family_name_kana", with: "シケン"
     fill_in "form_customer_given_name_kana", with: "ハナコ"
     fill_in "生年月日", with: "1970-01-01"
     choose "女性"
-    within("fleldset#home-address-fields") do
-      fill_in "郵便番号", with: "1000001"
-      select "東京都", from: "都道府県"
-      fill_in "市区町村",
-      fill_in
-      fill_in
-    end
-  end
-
-  scenario "職員が顧客、自宅住所、勤務先を追加する" do
-    click_link "顧客管理"
-    first("div.links").click_link "新規登録"
-
-    fill_in "メールアドレス", with: "test@example.jp"
-    fill_in "パスワード", with: "pw"
-    fill_in "form_customer_family_name", with: "試験"
-    fill_in "form_customer_given_name", with: "花子"
-    fill_in "form_customer_family_name_kana", with: "シケン"
-    fill_in "form_customer_given_name_kana", with: "ハナコ"
-    fill_in "生年月日", with: "1970-01-01"
-    choose "女性"
-    check "自宅住所を入力する"
     within("fieldset#home-address-fields") do
       fill_in "郵便番号", with: "1000001"
       select "東京都", from: "都道府県"
       fill_in "市区町村", with: "千代田区"
-      fill_in "町域、番地等", with: "千代田1-1-1"
+      fill_in "町域、番地等", with: "千代田 1-1-1"
       fill_in "建物名、部屋番号等", with: ""
     end
-    check "勤務先を入力する"
     within("fieldset#work-address-fields") do
       fill_in "会社名", with: "テスト"
       fill_in "部署名", with: ""
@@ -65,7 +41,7 @@ feature "職員による顧客管理" do
 
     new_customer = Customer.order(:id).last
     expect(new_customer.email).to eq("test@example.jp")
-    expect(new_customer.birthday).to eq(Date.new(1970, 1, 1))
+    expect(new_customer.birthday).to eq(Date.new(1970,1,1))
     expect(new_customer.gender).to eq("female")
     expect(new_customer.home_address.postal_code).to eq("1000001")
     expect(new_customer.work_address.company_name).to eq("テスト")
@@ -88,37 +64,22 @@ feature "職員による顧客管理" do
     expect(customer.email).to eq("test@example.jp")
     expect(customer.home_address.postal_code).to eq("9999999")
     expect(customer.work_address.company_name).to eq("テスト")
-  end
-
-  scenario "職員が生年月日と自宅の郵便番号に無効な値を入力する" do
-    click_link "顧客管理"
-    first("table.listing").click_link "編集"
-
-    fill_in "生年月日", with: "2100-01-01"
-    within("fieldset#home-address-fields") do
-      fill_in "郵便番号", with: "XYZ"
     end
-    click_button "更新"
 
-    expect(page).to have_css("header span.alert")
-    expect(page).to have_css(
-      "div.field_with_errors input#form_customer_birthday")
-    expect(page).to have_css(
-      "div.field_with_errors input#form_home_address_postal_code")
-  end
+    scenario "職員が生年月日と自宅の郵便番号に無効な値を入力する" do
+      click_link "顧客管理"
+      first("table.listing").click_link "編集"
 
-  scenario "職員が勤務先データのない既存顧客に会社名の情報を追加する" do
-    customer.work_address.destroy
-    click_link "顧客管理"
-    first("table.listing").click_link "編集"
+      fill_in "生年月日", with: "2100-01-01"
+      within("fieldset#home-address-fields") do
+        fill_in "郵便番号", with: "XYZ"
+      end
+      click_button "更新"
 
-    check "勤務先を入力する"
-    within("fieldset#work-address-fields") do
-      fill_in "会社名", with: "テスト"
+      expect(page).to have_css("header span.alert")
+      expect(page).to have_css(
+        "div.field_with_errors input#form_customer_birthday")
+      expect(page).to have_css(
+        "div.field_with_errors input#form_home_address_postal_code")
     end
-    click_button "更新"
-
-    customer.reload
-    expect(customer.work_address.company_name).to eq("テスト")
-  end
 end
